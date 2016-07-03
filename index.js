6ca29c16f1d650b52c6609f04c81f11c106b8e80
Nathan Rajlich <nathan@tootallnate.net>
2016-07-03 12:49:45 -0700

    preallocate the `results` Array
    Micro-optimization: since we know the size of the
    `results` array up-front, preallocate the Array
    with the proper `.length`.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = function mapTimes (count, callback) {
-  var results = []
+  var results = new Array(count)
   for (var i = 0; i < count; i++) {
-    results.push(callback(i))
+    results[i] = callback(i)
   }
   return results
 }
